db2a67f6bd1b82d34122c60e29d0e7a709c36c0f
Beatriz Martínez <beatriz@visualizados.com>
2022-11-16 11:40:50 +0100

    nav & whoami interaction

--- public/assets/main.js
+++ public/assets/main.js
@@ -1,26 +1,45 @@
 window.addEventListener('DOMContentLoaded', (event) => {
   
-  // - - - who am I sroll
-  function openWhoami() {
-    console.log('click')
-    const el = document.getElementById("whoami");
-    el.classList.add("top-0")
-    el.classList.remove("-top-full")
+  // - - - NAV MENU
+  function getMV() {
+    const h = document.getElementById("menu").clientHeight;
+    return `-mt-[${h}px]`;
   }
 
-  function closeWhoami() {
-    console.log('click')
-    const el = document.getElementById("whoami");
-    el.classList.remove("top-0")
-    el.classList.add("-top-full")
+
+  function loadNav() {
+    document.getElementById("menu").classList.add(getMV());
+    document.getElementById("menu").classList.remove("absolute", "-top-full"); 
+
+    setTimeout(() => {
+      document.getElementById("menu").classList.add("transition-[margin]", "transition-500");
+    }, "1000")
+  }
+  
+  function toggleNav() {
+    document.getElementById("menu").classList.toggle(getMV());
+    document.getElementById("menu").classList.toggle("mt-0");
+  }
+  function closeNav() {
+    document.getElementById("menu").classList.add(getMV());
+    document.getElementById("menu").classList.remove("mt-0");
   }
 
-  // Add event listener to table
-  const el = document.getElementById("link-whoami");
-  el.addEventListener("click", openWhoami, false);
+  // Apply interaction
+  loadNav();
+  document.getElementById("link-menu").addEventListener("click", toggleNav, false);
+  
+  // - - - WHO AM I
+  function toggleWhoami(e) {
+    const wai = document.getElementById("whoami");
+    wai.classList.toggle("top-0");
+    wai.classList.toggle("-top-full");
 
-  document.getElementById("close-whoami")
-  .addEventListener("click", closeWhoami, false);
-
-
+    if ( e.target.id == "close-whoami") closeNav();
+  }
+  
+  // Apply interaction
+  document.getElementById("home-whoami").addEventListener("click", toggleWhoami, false);
+  document.getElementById("nav-whoami").addEventListener("click", toggleWhoami, false);
+  document.getElementById("close-whoami").addEventListener("click", toggleWhoami, false);
 });
