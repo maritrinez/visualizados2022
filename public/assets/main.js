67ee26164fa4c51d3a5bc044c8ac5d28f8294867
Beatriz Martínez <beatriz@visualizados.com>
2023-04-27 15:31:37 +0200

    new projects grid design

--- public/assets/main.js
+++ public/assets/main.js
@@ -1,13 +1,13 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-  ////////////////////////
-  ///// MAIN ORDERS //////
-  ////////////////////////
+  ///////////////////////////
+  ///// ON LOAD ORDERS //////
+  ///////////////////////////
   
   // - - -  P R O J E C T S  - - -
   const csv_file = './assets/visualizados_projects.csv',
         csv_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQoVPZ9h7hN0Ckh41FKH2k42RaS-NGcJzHSVB_kl6GhF-AiaGHmm3JMwNfViiTDS0xeiIV-H0zxNGsd/pub?output=csv';
 
-  d3.csv(csv_file).then((data) => {
+  d3.csv(csv_url).then((data) => {
     // si esta en la home 
     loadProjects(data);
     // else
@@ -21,6 +21,11 @@ window.addEventListener('DOMContentLoaded', (event) => {
     }, 300);
   } 
 
+  // - - -  R E V E A L   W O R K  - - -
+  // Reveal visibles on load
+  revealWork();
+  // reveal nexts on scrolling
+  window.addEventListener("scroll", revealWork);
 
   // - - -  H I D D E N   E L E M E N T S   - - -
   firstLoad('whoami');
@@ -47,11 +52,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
     }, false);
   });
 
-
-
-
-  // - - PROJECT NAVIGATOR - -
-
   // - - RESIZE - -
   // https://www.cluemediator.com/how-to-wait-resize-end-event-and-then-perform-an-action-using-javascript
   let timeOutFunctionId;
@@ -74,6 +74,30 @@ window.addEventListener('DOMContentLoaded', (event) => {
       _setTansition('menu')
     }, 500);
   });
+
+  // - - GRID ANIMATION - -
+  function revealWork() {
+    if (isOpened.whoami) return;
+    const cards = document.querySelectorAll(".card"); 
+    for (let i = 0; i < cards.length; i++) {
+      const windowHeight = window.innerHeight;
+      const elementTop = cards[i].getBoundingClientRect().top,
+            elementBottom = cards[i].getBoundingClientRect().bottom;
+      const offset = windowHeight * 0.08; 
+      if (elementTop < windowHeight - offset & elementBottom > 0) { // reveals
+        cards[i].classList.add("active");
+      } else if (elementBottom < 0) { // hides upwards
+        cards[i].classList.remove("active");
+        cards[i].classList.remove("fade-up");
+        cards[i].classList.add("fade-down");
+      } else { // hide downwards
+        cards[i].classList.remove("active");
+        cards[i].classList.remove("fade-down");
+        cards[i].classList.add("fade-up");
+      }
+    }
+  }
+  
 
   // - - MAILTO - -
   // copy & reset tooltip message
@@ -255,66 +279,76 @@ function _rmAbsPos(id) {
 /////////////////////////////////////////////////////
 
 function loadProjects (data) {
-  const strength_icon = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-lime"><path stroke-linecap="round" stroke-linejoin="round" d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"/></svg>';
+  const strength_icon = '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-lime"><path stroke-linecap="round" stroke-linejoin="round" d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"></path></svg>';
 
   const classes = {
-      project_item: 'projects_item group relative',
-      project_image: 'project_image aspect-w-1 aspect-h-1 w-full overflow-hidden',
-      image: 'h-full w-full object-cover object-center',
-      overlay: 'overlay absolute inset-0 flex flex-col justify-center bg-indigo opacity-0 group-hover:bg-indigo/70 group-hover:opacity-100 transition-opacity',
-      overlay_copy: 'text-white flex flex-col items-center',
-      strength: 'flex space-x-1'
+      card_odd: 'card fade-up group',
+      card_even: 'card fade-up group sm:delay-300',
+      card_image: 'figure h-min overflow-hidden',
+      image: 'h-full w-full object-cover object-center group-hover:scale-[1.03] transition-all duration-300',
+      caption: 'caption text-right',
+      caption_title: 'title flex space-x-2 justify-end items-center mt-2',
+      caption_title_line: 'w-1/4 border-b border-black transition-all duration-700 group-hover:w-full',
+      caption_title_text: 'font-sans font-medium whitespace-nowrap',
+      caption_subtitle: 'subtitle h-8 overflow-hidden transition-all',
+      caption_subtitle_strength: 'strength flex space-x-2 justify-end items-end transition-all duration-300 mt-[-2.2rem] group-hover:mt-[-0.2rem]',
+      caption_subtitle_client: 'client p-small before:inline-block before:border-l-[1px] before:border-black before:h-1/4  before:m-3'
   }
 
   // sort the data by order
   data.sort((a, b) => a.index - b.index)
 
   // append a div for each project
-  let items = d3.select('#work').select('.layout').selectAll('a.projects_item')
+  let items = d3.select('#work').select('ul').selectAll('li.card')
       .data(data)
-      .join('a')
-      .attr('class', classes.project_item)
-      .attr('href', d => `./${d.project_path}.html`);
+      .join('li')
+      .attr('class', (d,i) => (i%2 == 0) ? classes.card_odd : classes.card_even);
   
-  items.selectAll('.project_image')
+  items.selectAll('.figure')
       .data(d => [d])
       .join('div')
-      .attr('class', classes.project_image)
-  .selectAll('img')
+      .attr('class', classes.card_image)
+    .selectAll('a')
+      .data(d => [d])
+      .join('a')
+      .attr('src', d => `./assets/images/scs/${d.project_path}`)
+    .selectAll('img')
       .data(d => [d])
       .join('img')
       .attr('class', classes.image)
       .attr('src', d => `./assets/images/scs/${d.img_path}`);
   
-  let overlay = items.selectAll('.overlay')
+  let caption = items.selectAll('.caption')
       .data(d => [d])
       .join('div')
-      .attr('class', classes.overlay)
-      .selectAll('div')
+      .attr('class', classes.caption);
+  
+  caption.selectAll('.title')
       .data(d => [d])
       .join('div')
-      .attr('class', classes.overlay_copy);
-  overlay.selectAll('h2')
-      .data(d => [d])
-      .join('h2')
-      .attr('class', 'uppercase')
-      .html(d => d.title)
+      .attr('class', classes.caption_title)
+    .selectAll('div')
+      .data(d => [classes.caption_title_line, d])
+      .join('div')
+      .attr('class', d => (d.title) ? classes.caption_title_text : d)
+      .html(d => (d.title) ? d.title : '')
   
-  overlay.selectAll('h3')
-      .data(d => [d])
-      .join('h3')
-      .html(d => d.client)
-  
-  let strength = overlay.selectAll('div')
+  let subtitle = caption.selectAll('.subtitle')
       .data(d => [d])
       .join('div')
-      .attr('class', classes.strength)
-      .html(strength_icon)
+      .attr('class', classes.caption_subtitle);
   
-  strength.selectAll('h3')
+  subtitle.selectAll('.strength')
     .data(d => [d])
-    .join('h4')
-    .html(d => d.strength)
+    .join('div')
+    .attr('class', classes.caption_subtitle_strength)
+    .html(d => `${strength_icon}<p class="p-small font-medium">${d.strength}</p>`);
+  
+  subtitle.selectAll('.client')
+    .data(d => [d])
+    .join('div')
+    .attr('class', classes.caption_subtitle_client)
+    .html(d => d.client);
 }
 
 // - - - COPY MAIL
